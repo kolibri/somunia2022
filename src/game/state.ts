bddf33215e1942ad256c22199ba9b8b1c030c1ea
ko.libri <entengelb@gmail.com>
2022-08-11 00:19:36 +0200

    fixed the test by implementing gameState

--- src/game/state.ts
+++ src/game/state.ts
@@ -1,11 +1,20 @@
 "use strict"
 
 export interface GameStateInterface {
-	get(key: string): any;
-	set(key: string, value: any): any;
+    get(key: string): any;
+    set(key: string, value: any): any;
 }
 
 export class GameState implements GameStateInterface {
-	get(key: string) {}
-	set(key: string, value: any){}
+    state: {[key: string]: any} = {}
+    get(key: string) {
+        if (!(key in this.state)) {
+            return // @todo: throw error
+        }
+
+        return this.state[key]
+    }
+    set(key: string, value: any){
+        this.state[key] = value
+    }
 }
